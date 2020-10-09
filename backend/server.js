e89ea8bf0875db76a0d8470ed7426e8fe9892600
Andrii Suboch <andrii.suboch@gmail.com>
2020-10-09 20:10:02 +0300

    init backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+require('./database');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const users = require('/api/users');
+app.use('/api/users', users);
+
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
+
+// mongodb+srv://ggandr:<password>@forumcluster.h663z.mongodb.net/<dbname>?retryWrites=true&w=majority
